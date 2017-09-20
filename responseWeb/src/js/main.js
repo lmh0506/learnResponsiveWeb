e9edbe5f19dfa2e48632bfe0d46fb0206ec9dc2a
lmh <495453865@qq.com>
2017-09-20 16:17:54 +0800

    响应式网页

---
+++ responseWeb/src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true
+  })
+})
